ac8bb54933562fa59eefbac5bed056d48d61a8fe
Yawenina <xiaoywkate@163.com>
2017-09-14 22:32:58 +0800

    add bubble sort

---
+++ sortAndSearch/bubbleSort.js
@@ -0,0 +1,13 @@
+export default function bubbleSort(array) {
+  const len = array.length;
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < (len - i - 1); j++) {
+      if (array[j] > array[j + 1]) {
+        const temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+  return array;
+}
--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 import test from 'ava';
 
 import binarySearch from './sortAndSearch/binarySearch';
+import bubbleSort from './sortAndSearch/bubbleSort';
 
 test('binary search: no elements', (t) => {
   t.is(binarySearch([], 10), -1);
@@ -20,4 +21,9 @@ test('binary search: not exists', (t) => {
 
 test('binary search: exists', (t) => {
   t.is(binarySearch([1, 2, 3, 10, 15], 10), 3);
+});
+
+test('bubble sort: correct', (t) => {
+  t.deepEqual(bubbleSort([33, 103, 3, 726, 200, 984, 198, 764, 9]),
+    [3, 9, 33, 103, 198, 200, 726, 764, 984]);
 });
